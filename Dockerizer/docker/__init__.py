f45fd616959f87798b4b3d401509a2610397d4b7
mrstephenneal <stephen.neal14@gmail.com>
2019-05-10 11:26:49 -0400

    MAKE morning pull script for pulling a list of Docker images

--- Dockerizer/docker/__init__.py
+++ Dockerizer/docker/__init__.py
@@ -1,5 +1,5 @@
-from Dockerizer.docker.docker import Docker, DockerCommands
+from Dockerizer.docker.docker import Docker, DockerCommands, unpack_image_name
 from Dockerizer.docker.gui import gui
 from Dockerizer.docker.run import Dockerrun
 
-__all__ = ['Docker', 'Dockerrun', 'gui']
+__all__ = ['Docker', 'DockerCommands', 'Dockerrun', 'gui', 'unpack_image_name']
--- Dockerizer/morning_pull.py
+++ Dockerizer/morning_pull.py
@@ -1,14 +1,29 @@
+from looptools import Timer
 from databasetools import JSON
 from Dockerizer.config import MORNING_PULL_JSON
-from Dockerizer.docker import Docker
+from Dockerizer.docker import Docker, unpack_image_name
+
+
+@Timer.decorator
+def morning_pull():
+    print('\nMorning pull image list path: {0}\n'.format(MORNING_PULL_JSON))
+    to_pull = JSON(MORNING_PULL_JSON).read()['images']
+
+    if len(to_pull) > 0:
+        print('Pulling {0} images from DockerHub:\n'.format(len(to_pull)))
+        for pull in to_pull:
+            Docker(**unpack_image_name(pull)).pull()
+    else:
+        print('No images to pull.  Add images to morning_pull.json.')
+
+    print('\nAvailable Docker Images:')
+    for i, image in enumerate(Docker().images):
+        print('\t{0}: {1}'.format(i, image))
+    print('\n')
 
 
 def main():
-    print('Morning pull image list path:\n{0}'.format(MORNING_PULL_JSON))
-    to_pull = JSON(MORNING_PULL_JSON).read()['images']
-
-    if to_pull > 0:
-        for pull in to_pull
+    morning_pull()
 
 
 if __name__ == '__main__':
