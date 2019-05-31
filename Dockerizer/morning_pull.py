6337e6057e9c3924825e9751f057fa95585a245d
mrstephenneal <stephen.neal14@gmail.com>
2019-05-31 10:56:55 -0400

    FIX print statements

--- Dockerizer/morning_pull.py
+++ Dockerizer/morning_pull.py
@@ -4,25 +4,28 @@ from Dockerizer.config import MORNING_PULL_JSON
 from Dockerizer.docker import Docker, unpack_image_name
 
 
+def list_print(items, head=None):
+    if head:
+        print(head)
+    for i, item in enumerate(items):
+        print('\t\t{0:2}: {1}'.format(i + 1, item))
+
+
 @Timer.decorator
 def morning_pull():
     to_pull = JSON(MORNING_PULL_JSON).read()['images']
     print('Morning Pull - Docker image pull utility')
     print('\tlist path: {0}'.format(MORNING_PULL_JSON))
-    print('\timages to pull:')
-    for i, img in enumerate(to_pull):
-        print('\t\t{0:2}: {1}'.format(i + 1, img))
+    list_print(to_pull, '\timages to pull:')
 
     if len(to_pull) > 0:
         for pull in to_pull:
             Docker(**unpack_image_name(pull)).pull()
     else:
         print('No images to pull.  Add images to morning_pull.json.')
-
-    print('\nAvailable Docker Images:')
-    for i, image in enumerate(Docker().images):
-        print('\t{0}: {1}'.format(i, image))
-    print('\n')
+    images = Docker().images
+    list_print(images, '\nAvailable Docker Images:')
+    return images
 
 
 def main():
