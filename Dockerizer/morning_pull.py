77633d807186fa78223c388a8c0589940d36ad41
mrstephenneal <stephen.neal14@gmail.com>
2019-06-19 10:14:49 -0400

    OPTIMIZE morning_pull module by converting functions to singular class

--- Dockerizer/morning_pull.py
+++ Dockerizer/morning_pull.py
@@ -5,59 +5,91 @@ from Dockerizer.config import MORNING_PULL_JSON
 from Dockerizer.docker import Docker, unpack_image_name
 
 
-def list_print(items, head=None):
-    """Print a list of items."""
-    if head:
-        print(head)
-    for i, item in enumerate(items):
-        print('\t{0:2}: {1}'.format(i + 1, item))
+class MorningPull:
+    def __init__(self, json_path=MORNING_PULL_JSON):
+        self.json_path = json_path
+        self._json = None
+        self._existing_images = None
 
+    @property
+    def json(self):
+        """Return JSON object with morning_pull.json available."""
+        if not self._json:
+            self._json = JSON(self.json_path)
+        return self._json
 
-def mp_json(json_path=MORNING_PULL_JSON):
-    """Return JSON object with morning_pull.json available."""
-    return JSON(json_path)
+    @property
+    def existing_images(self):
+        if not self._existing_images:
+            self._existing_images = sorted(self.json.read()['images'])
+        return self._existing_images
 
+    @property
+    def images_dict(self):
+        """Return a dictionary of enumerated Docker images with ID keys and Docker image values."""
+        return {i + 1: item for i, item in enumerate(self.existing_images)}
 
-def add_image(image, json_path=MORNING_PULL_JSON):
-    """Add a Docker Image to the morning_pull.json list."""
-    mp_json(json_path).append(key='images', data=image)
+    def pull(self):
+        to_pull = self.existing_images
+        print('Morning Pull - Docker image pull utility')
+        print('\tlist path: {0}'.format(MORNING_PULL_JSON))
+        self.print('Docker Images to pull:')
 
+        if len(to_pull) > 0:
+            with Timer('morning-pull'):
+                for pull in to_pull:
+                    Docker(**unpack_image_name(pull)).pull()
+        else:
+            print('No images to pull.  Add images to morning_pull.json.')
+        images = Docker().images
+        self.print('\nAvailable Docker Images:')
+        return images
 
-def remove_image(image, json_path=MORNING_PULL_JSON):
-    """Remove a Docker Image from the morning_pull.json list."""
-    images = get_mp_list()
-    images.pop(images.index(image))
-    mp_json(json_path).update(key='images', data=images)
+    def print(self, msg='morning-pull Docker Image list:'):
+        """Print a list of items."""
+        if msg:
+            print(msg)
+        for i, item in enumerate(self.existing_images):
+            print('\t{0:2}: {1}'.format(i + 1, item))
 
+    def _add(self, image):
+        """Add a Docker Image to the morning_pull.json list."""
+        self.json.append(key='images', data=image)
 
-def get_mp_list():
-    return sorted(mp_json().read()['images'])
+    def add(self, images_to_add):
+        print('Added the following Docker Image(s) to the morning pull list:')
+        for image in images_to_add:
+            if image not in self.json.read()['images']:
+                print('\t{0}'.format(image))
+                self._add(image.strip())
 
+    def _remove(self, image):
+        """Remove a Docker Image from the morning_pull.json list."""
+        self.existing_images.pop(self.existing_images.index(image))
+        self.json.update(key='images', data=self.existing_images)
 
-def print_morning_pull(msg='morning-pull Docker Image list:'):
-    list_print(get_mp_list(), msg)
+    def remove(self, images_to_remove):
+        print('Removing the following Docker Image(s) from the morning pull list:')
+        for image in images_to_remove:
+            print('\t{0}'.format(image))
+            self._remove(image.strip())
 
-
-def enumerate_images(images=None):
-    """Return a dictionary of enumerated Docker images with ID keys and Docker image values."""
-    return {i + 1: item for i, item in enumerate(get_mp_list() if not images else images)}
-
-
-@Timer.decorator
-def morning_pull():
-    to_pull = get_mp_list()
-    print('Morning Pull - Docker image pull utility')
-    print('\tlist path: {0}'.format(MORNING_PULL_JSON))
-    list_print(to_pull, 'Docker Images to pull:')
-
-    if len(to_pull) > 0:
-        for pull in to_pull:
-            Docker(**unpack_image_name(pull)).pull()
-    else:
-        print('No images to pull.  Add images to morning_pull.json.')
-    images = Docker().images
-    list_print(images, '\nAvailable Docker Images:')
-    return images
+    def edit(self):
+        self.print()
+        print('\nEnter the ID(s) of the image(s) you would like to remove separated by spaced')
+        removals = input('Image(s) to remove: ')
+        if len(removals) > 0:
+            image_ids = list(map(int, removals.split(' ')))
+            print(image_ids)
+            if len(image_ids) > 0:
+                for i_id, img in self.images_dict.items():
+                    print(i_id, img)
+                    if i_id in image_ids:
+                        print('\tRemoving', img.strip())
+                        self._remove(img.strip())
+                self.print('\nNew morning-pull Docker image list:')
+        else:
+            print('No images to remove.')
 
 
 def main():
@@ -79,45 +111,29 @@ def main():
     ap.add_argument('--edit', help=helpers['edit'], action='store_true')
     args = vars(ap.parse_args())
 
+    # Initialize MorningPull
+    mp = MorningPull()
+
     # Run morning pull if no additional arguments were passed
     if all(k in (None, False) for k in args.values()):
         print('Morning pull')
-        morning_pull()
+        mp.pull()
     else:
         # Display the morning pull list
         if args['list']:
-            print_morning_pull()
+            mp.print()
 
         # Add images to morning pull list
         if args['add']:
-            print('Added the following Docker Image(s) to the morning pull list:')
-            for image in args['add']:
-                if image not in mp_json().read()['images']:
-                    print('\t{0}'.format(image))
-                    add_image(image.strip())
+            mp.add(args['add'])
 
         # Remove images from the morning pull list
         if args['remove']:
-            print('Removing the following Docker Image(s) from the morning pull list:')
-            for image in args['remove']:
-                print('\t{0}'.format(image))
-                remove_image(image.strip())
+            mp.remove(args['remove'])
 
         # Edit the morning-pull Docker image list
         if args['edit']:
-            print_morning_pull()
-            print('\nEnter the ID(s) of the image(s) you would like to remove separated by spaced')
-            removals = input('Image(s) to remove: ')
-            if len(removals) > 0:
-                image_ids = list(map(int, removals.split(' ')))
-                if len(image_ids) > 0:
-                    for i_id, img in enumerate_images().items():
-                        if i_id in image_ids:
-                            print('\tRemoving', img.strip())
-                            remove_image(img.strip())
-                    print_morning_pull('\nNew morning-pull Docker image list:')
-            else:
-                print('No images to remove.')
+            mp.edit()
 
 
 if __name__ == '__main__':
